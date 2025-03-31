4b325ce921b45c3906de5b8706948e8121954da7
Well <welliaquimquim@gmail.com>
2025-03-31 00:20:48 -0400

    wip: aparência finalizada.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import Button from "./Components/Button";
 import "./input.css";
 
 function App() {
-  function restart() {
-    return alert("clicou");
-  }
   return (
     <>
-      <div className="bg-gray-700 w-full h-screen">
-        <Button variant="primary">Primary</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="danger">Danger</Button>
+      <div className="bg-gray-700 w-full h-screen flex justify-center items-center">
+        <div className="bg-gray-950 w-[450px] h-52 flex justify-center items-center rounded-md border-2 border-gray-800 gap-4">
+          <Button variant="primary" text="Primary"></Button>
+          <Button variant="secondary" text="Secondary"></Button>
+          <Button variant="danger" text="Danger"></Button>
+          <Button variant="sucess" text="Sucess"></Button>
+        </div>
       </div>
     </>
   );
--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -2,9 +2,26 @@ import { ButtonHTMLAttributes } from "react";
 import clsx from "clsx";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  variant?: "primary" | "secondary" | "danger";
+  variant?: "primary" | "secondary" | "danger" | "sucess";
+  text: string;
 };
 
-export default function Button<ButtonProps>({}) {
-  return <button className={clsx("")}></button>;
+export default function Button({ text, variant = "primary" }: ButtonProps) {
+  const variantClasses = {
+    primary: "bg-blue-500 hover:bg-blue-600",
+    secondary: "bg-gray-500 hover:bg-gray-600",
+    danger: "bg-red-500 hover:bg-red-600",
+    sucess: "bg-green-700 hover:bg-green-800",
+  };
+  return (
+    <button
+      className={clsx(
+        "px-4 py-2 rounded-md font-semibold text-white",
+        variantClasses[variant]
+      )}
+      onClick={() => alert(`Você clicou em ${variant}`)}
+    >
+      {text}
+    </button>
+  );
 }
